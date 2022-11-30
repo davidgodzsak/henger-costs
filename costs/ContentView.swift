f977bacedc956857705d5e1a663a12af1f62ec72
Dávid Godzsák <godzsakdavid@gmail.com>
2022-11-30 11:29:33 +0100

    Initial Commit

---
+++ costs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  costs
+//
+//  Created by Dávid Godzsák on 30/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ costs/costsApp.swift
@@ -0,0 +1,17 @@
+//
+//  costsApp.swift
+//  costs
+//
+//  Created by Dávid Godzsák on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct costsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
